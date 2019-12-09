990df7d84c742cd7b67a43604959493eba6722ab
Oussama <omjihil@asu.edu>
2019-12-09 10:38:56 +0100

    Initial Commit

---
+++ src/test/java/enadev/spring/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,14 @@
+package enadev.spring.sfgpetclinic;
+
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
