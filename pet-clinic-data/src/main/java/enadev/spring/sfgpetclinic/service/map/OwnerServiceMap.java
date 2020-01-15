de09da8c74f873f99c7e330ebd2ce08c239b0c6c
Oussama <omjihil@asu.edu>
2020-01-15 11:04:17 +0100

    Controller and Index page created. Closes #8

---
+++ pet-clinic-data/src/main/java/enadev/spring/sfgpetclinic/service/map/OwnerServiceMap.java
@@ -0,0 +1,33 @@
+package enadev.spring.sfgpetclinic.service.map;
+
+import enadev.spring.sfgpetclinic.model.Owner;
+import enadev.spring.sfgpetclinic.service.CrudInterface;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudInterface<Owner,Long> {
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
