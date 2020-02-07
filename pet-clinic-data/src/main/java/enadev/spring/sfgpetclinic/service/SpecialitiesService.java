1e79fa234c6d7401ab55abb4a48d9d566aa007e6
Oussama <omjihil@asu.edu>
2020-02-07 18:22:12 +0100

    Secialities Map Service added.  Closes #31

---
+++ pet-clinic-data/src/main/java/enadev/spring/sfgpetclinic/service/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package enadev.spring.sfgpetclinic.service;
+
+import enadev.spring.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudInterface<Speciality, Long> {
+}
