3485cd9d7279d112531794dc1673c9308e0a4482
Oussama <omjihil@asu.edu>
2020-02-13 16:30:37 +0100

    Owners converted.  Closes #36

--- pet-clinic-data/src/main/java/enadev/spring/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/enadev/spring/sfgpetclinic/model/Pet.java
@@ -1,12 +1,21 @@
 package enadev.spring.sfgpetclinic.model;
 
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
+    @Column(name = "name")
     private String name;
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+    @Column(name = "birth_day")
     private LocalDate birthDay;
 
     public PetType getPetType() {
