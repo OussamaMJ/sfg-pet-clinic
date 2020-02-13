357bcef0e968613ac17b88321537b8076d46dbf1
Oussama <omjihil@asu.edu>
2020-02-13 16:45:07 +0100

    Vets converted.  Closes #37

--- pet-clinic-data/src/main/java/enadev/spring/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/enadev/spring/sfgpetclinic/model/Speciality.java
@@ -1,6 +1,13 @@
 package enadev.spring.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
