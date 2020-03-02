dce698c9ac07a2ae7a6ed06dba4a957d03f1a013
Oussama <omjihil@asu.edu>
2020-03-02 21:15:18 +0100

    Upgraded Junit to Version 5.  Closes #4

--- pet-clinic-web/src/test/java/enadev/spring/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/enadev/spring/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,13 +1,19 @@
 package enadev.spring.sfgpetclinic;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SfgPetClinicApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	@DisplayName("1 + 1 = 2")
+	void addsTwoNumbers() {
+		Assertions.assertEquals(2, 1+1, "1 - 1 should equal 2");
 	}
-
 }
