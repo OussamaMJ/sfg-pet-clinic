afd025d076c169b901f12350ea40020a75f35a59
Oussama <omjihil@asu.edu>
2020-03-13 10:57:29 +0100

    Added visit Controller and Mapping #57

--- pet-clinic-data/src/main/java/enadev/spring/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/enadev/spring/sfgpetclinic/model/Pet.java
@@ -36,6 +36,8 @@ public class Pet extends BaseEntity{
         this.petType = petType;
         this.owner = owner;
         this.birthDay = birthDay;
-        this.visits = visits;
+        if(visits != null || visits.size() > 0) this.visits = visits;
     }
+
+
 }
